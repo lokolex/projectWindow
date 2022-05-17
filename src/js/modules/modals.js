aef625c99dd245b66ee922f31a1a61b24c93a43e
Alexei <lokolex310188@gmail.com>
2022-05-17 04:07:10 +0400

    finished project

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -5,6 +5,7 @@ const modals = (state) => {
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
     const windows = document.querySelectorAll('[data-modal]');
+    const scroll = calcScroll();
 
     let kHelp = 1;
 
@@ -15,6 +16,7 @@ const modals = (state) => {
 
       modal.style.display = 'block';
       document.body.style.overflow = 'hidden';
+      document.body.style.marginRight = `${scroll}px`;
     };
 
     const messageRemove = () => {
@@ -73,6 +75,7 @@ const modals = (state) => {
 
       modal.style.display = 'none';
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
     });
 
     modal.addEventListener('click', (e) => {
@@ -82,6 +85,7 @@ const modals = (state) => {
         });
         modal.style.display = 'none';
         document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
       }
     });
 
@@ -92,6 +96,7 @@ const modals = (state) => {
         });
         modal.style.display = 'none';
         document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
       }
     });
   }
@@ -102,6 +107,20 @@ const modals = (state) => {
       document.body.style.overflow = 'hidden';
     }, time);
   };
+
+  function calcScroll() {
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
 
   
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
